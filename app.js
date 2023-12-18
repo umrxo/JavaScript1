db3bcb0f06e8bcfd3099f50ee0051f68b43b33ee
umar <unarmedq267@gmail.com>
2023-12-19 01:41:43 +0500

    JavaScript

---
+++ app.js
@@ -0,0 +1,48 @@
+//first start
+
+var yourName = prompt("Введите ваше имя");
+
+console.log("Меня зовут " + yourName);
+
+var yourAge = prompt("Введиту ваш возраст");
+
+console.log("Мой возраст " + yourAge);
+
+
+var solve1 = +prompt("Решите пример 10 + 5 = ?");
+
+console.log("Правильный ответ: 10 + 5 = " + (10 + 5) + " Ваш ответ: " + solve1);
+
+
+var solve2 = +prompt("Решите пример 20 - 5 = ?");
+
+console.log("Правильный ответ: 20 - 5 = " + (20 - 5) + " Ваш ответ: " + solve2);
+
+
+var solve3 = +prompt("Решите пример 3 * 5 = ?");
+
+console.log("Правильный ответ: 3 * 5 = " + 3 * 5 + " Ваш ответ: " + solve3);
+
+
+var solve4 = +prompt("Решите пример 30 / 2 = ?");
+
+console.log("Правильный ответ: 30 / 2 = " + 30 / 2 + " Ваш ответ: " + solve4);
+
+
+var solve5 = +prompt("Решите пример 12 % 5 = ?");
+
+console.log("Правильный ответ: 12 % 5 = " + (12 % 5) + " Ваш ответ: " + solve5);
+
+//first end
+
+var cont = prompt("Напишите что-то чтобы продолжить");
+
+//second start
+
+var num1 = +prompt("Введите первое число");
+var num2 = +prompt("Введите второе число");
+var num3 = +prompt("Введите третье число");
+
+alert("Среднее арифметическое = " + (num1 + num2 + num3) / 3);
+
+//second end
